f3557d9e3b176b6766729139cb0d77bd2daf0303
Esteban Durán <eiduran@estudiantec.cr>
2021-03-23 00:18:10 -0600

    Created the java application
    Added two clases
    Working gui for transitioning between panels

---
+++ ClueGuess/src/clueguess/ClueGuess.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clueguess;
+
+/**
+ *
+ * @author eidur
+ */
+public class ClueGuess {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        clientWindow pantalla = new clientWindow();
+        pantalla.setVisible(true); 
+               
+
+    }
+    
+}
