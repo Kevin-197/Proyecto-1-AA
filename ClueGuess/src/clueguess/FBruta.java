2cdcb11e748d855563e3d4a1ada594221ce643c4
Esteban Durán <eiduran@estudiantec.cr>
2021-03-30 23:23:41 -0600

    Refined Brute Force
    Brute force now skips cards that have already been declared as incorrect

--- ClueGuess/src/clueguess/FBruta.java
+++ ClueGuess/src/clueguess/FBruta.java
@@ -17,6 +17,7 @@ public class FBruta extends Thread{
     public ArrayList<Carta> Motivos = new ArrayList<Carta>();
     public ArrayList<Carta> PCuerpo = new ArrayList<Carta>();
     public ArrayList<Carta> Lugares = new ArrayList<Carta>();
+    public ArrayList<Carta> Descartadas = new ArrayList<Carta>();
     private ArrayList<javax.swing.JLabel> Displays;
     Combinacion propuesta;
     Combinacion Solucion;
@@ -46,23 +47,35 @@ public class FBruta extends Thread{
     }
     public boolean excecute(){
         Carta incorrecta = Solucion.verificar(propuesta, this.Displays);
-        for (int i = 0; i < Sospechosos.size() && incorrecta != null; i++) {
-            propuesta.setSospechoso(Sospechosos.get(i));
-            for (int j = 0; j < Armas.size() && incorrecta != null; j++) {
-                propuesta.setArma(Armas.get(j));
-                for (int k = 0; k < Motivos.size() && incorrecta != null; k++) {
-                    propuesta.setMotivo(Motivos.get(k));
-                    for (int l = 0; l < PCuerpo.size() && incorrecta != null; l++) {
-                        propuesta.setParte(PCuerpo.get(l));
-                        for (int m = 0; m < Lugares.size(); m++) {
-                            propuesta.setLugar(Lugares.get(m));
-                            incorrecta = Solucion.verificar(propuesta, this.Displays);
-                            if(incorrecta == null){
-                                break;
+        for (int i = 0; i < Sospechosos.size() && incorrecta != null; i++ ) {
+            if(!(this.Descartadas.contains(Sospechosos.get(i)))){
+                    propuesta.setSospechoso(Sospechosos.get(i));
+                    for (int j = 0; j < Armas.size() && incorrecta != null; j++) {
+                        if(!(this.Descartadas.contains(Armas.get(j)))){
+                            propuesta.setArma(Armas.get(j));
+                            for (int k = 0; k < Motivos.size() && incorrecta != null; k++) {
+                                if(!(this.Descartadas.contains(Motivos.get(k)))){
+                                    propuesta.setMotivo(Motivos.get(k));
+                                    for (int l = 0; l < PCuerpo.size() && incorrecta != null; l++) {
+                                        if(!(this.Descartadas.contains(PCuerpo.get(l)))){
+                                            propuesta.setParte(PCuerpo.get(l));
+                                            for (int m = 0; m < Lugares.size(); m++) {
+                                                if(!(this.Descartadas.contains(Lugares.get(m)))){
+                                                    propuesta.setLugar(Lugares.get(m));
+                                                    incorrecta = Solucion.verificar(propuesta, this.Displays);
+                                                    if(incorrecta == null){
+                                                        break;
+                                                    }else{
+                                                        this.Descartadas.add(incorrecta);
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
                             }
                         }
                     }
-                }
             }
         }  
         return true;
